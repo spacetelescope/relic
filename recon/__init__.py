6173d90b0526ee013522cacd66a36fd69e4fa8a3
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-26 22:13:00 -0400

    Fix improper output

--- recon/__init__.py
+++ recon/__init__.py
@@ -25,6 +25,7 @@ import sys
 try:
     from .version import *
 except ImportError:
-    print('First run detected.')
+    # If we are imported via submodule, ignore RECON's internal version data.
+    pass
 
 PY3 = sys.version_info[0] == 3

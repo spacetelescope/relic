fd2736e95dc43e8ee5965c63ad57030f533087b1
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2018-05-22 08:42:17 -0400

    Handle no git (#2)
    * Handle case where git is not installed
    
    * Show warnings when git is not installed

--- relic/git.py
+++ relic/git.py
@@ -1,6 +1,11 @@
+from __future__ import print_function
+
+
 import re
+import sys
 from collections import namedtuple
 from subprocess import Popen, PIPE
+from distutils.spawn import find_executable
 from . import PY3
 from . import ABBREV
 
@@ -18,6 +23,13 @@ def strip_dirty(tag):
 
 
 def git(*commands):
+    # If git is not available on PATH, die early
+    if not find_executable('git'):
+        print("warning: RELIC requires 'git' (see: https://git-scm.com)\n"
+              "warning: 'git' is not required for archive-based installation "
+              "(i.e., pip, pypi)", file=sys.stderr)
+        return None
+
     command = ['git', '--no-pager'] + [c for c in commands]
     proc = Popen(command, stdout=PIPE, stderr=PIPE, stdin=PIPE)
 
